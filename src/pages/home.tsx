ec37c145c309c382a716036f44ec9e6bc22314ab
TingjunWan <wtj9631@163.com>
2021-07-01 20:45:56 +0800

    chart10 完成

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,6 +11,7 @@ import {Chart6} from "../components/chart-6";
 import {Chart7} from '../components/chart-7';
 import {Chart8} from '../components/chart-8';
 import {Chart9} from '../components/chart-9';
+import {Chart10} from "../components/chart-10";
 
 export const Home = () => {
     const year = new Date().getFullYear();
@@ -44,7 +45,7 @@ export const Home = () => {
                 <div className="bordered row1 案发类型">
                     <h2>案发类型统计</h2>
                     <div className="charts">
-
+                        <Chart10/>
                     </div>
                 </div>
                 <div className="bordered row2 案发街道">
